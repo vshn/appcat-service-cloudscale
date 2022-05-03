859dd55cec61051ed1662de9d0fab26c014f3963
David Gubler <david.gubler@vshn.ch>
2022-05-03 10:46:06 +0200

    add missing config file

---
+++ config/cloudscale_objects_user/config.go
@@ -0,0 +1,10 @@
+package cloudscale_objects_user
+
+import "github.com/crossplane/terrajet/pkg/config"
+
+// Configure configures individual resources by adding custom ResourceConfigurators.
+func Configure(p *config.Provider) {
+    p.AddResourceConfigurator("cloudscale_objects_user", func(r *config.Resource) {
+	r.ExternalName = config.IdentifierFromProvider
+    })
+}
