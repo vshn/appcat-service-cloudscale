52f8348fc861ab5bd1a553b3d0d935980aece999
Simon Beck <simon.beck@vshn.ch>
2022-05-12 15:16:38 +0200

    Map the connection details to the Crossplane ConnectionSecret
    This commit adds a map for adding the S3 credentials to a Crossplane
    ConnectionSecret.
    
    The secret is currently still visible in the status field of the user.

--- config/cloudscaleobjectsuser/config.go
+++ config/cloudscaleobjectsuser/config.go
@@ -1,10 +1,40 @@
 package cloudscaleobjectsuser
 
-import "github.com/crossplane/terrajet/pkg/config"
+import (
+	"fmt"
+
+	"github.com/crossplane/terrajet/pkg/config"
+)
 
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("cloudscale_objects_user", func(r *config.Resource) {
 		r.ExternalName = config.IdentifierFromProvider
+		r.Sensitive.AdditionalConnectionDetailsFn = func(attr map[string]interface{}) (map[string][]byte, error) {
+			return extractMapFromInterfaceSlice(attr["keys"]), nil
+		}
 	})
+}
+
+func extractMapFromInterfaceSlice(t interface{}) map[string][]byte {
+	listOfMaps := map[string][]byte{}
+
+	// test if this is a slice
+	if interfaceValue, ok := t.([]interface{}); ok {
+		// loop over the slice
+		for index, potentialMap := range interfaceValue {
+			// check if entry is a map
+			if realMap, ok := potentialMap.(map[string]interface{}); ok {
+				// convert map[string]interface{} to map[string]string
+				for key, value := range realMap {
+					// check if value is a string
+					if valueString, ok := value.(string); ok {
+						listOfMaps[fmt.Sprintf("%s_%d", key, index)] = []byte(valueString)
+					}
+				}
+			}
+		}
+	}
+
+	return listOfMaps
 }
